ed8285b899c8d4381a25c743da6583694d674048
Francesco Doria <doria5@live.it>
2026-07-02 00:29:48 +0300

    step 1

--- src/main/java/lv/bootcamp/shelter/service/CsvImportService.java
+++ src/main/java/lv/bootcamp/shelter/service/CsvImportService.java
@@ -5,8 +5,15 @@ import lv.bootcamp.shelter.model.Animal;
 import lv.bootcamp.shelter.service.data.ImportResult;
 
 import java.nio.file.Path;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.Month;
 
 @Slf4j
 public class CsvImportService {
@@ -19,6 +26,51 @@ public class CsvImportService {
         // TODO Step 1:
         // 1) Read intake.csv with UTF-8.
         // 2) Skip header row.
+        try (Stream<String> lines = Files.lines(inputPath, StandardCharsets.UTF_8)) {
+            lines.skip(1).forEach(line -> {
+                String[] parts = line.split(",");
+
+                String name = parts[0];
+                if (name.trim().isEmpty()){
+                    log.warn("Skipping string because no name was provided");
+                    return;
+                }
+
+                String animal = parts[1];
+                if (animal.trim().isEmpty()){
+                    log.warn("Skipping string because no animal species was provided");
+                    return;
+                }
+
+                Integer age;
+                try {
+                    String ageString = parts[2].trim();
+                    age = ageString.isEmpty() ? null : Integer.parseInt(ageString);
+                } catch (NumberFormatException e){
+                    log.warn("Catching NumberFormatException: [{}] and skipping row.", e.getMessage());
+                    return;
+                }
+
+                boolean adopted = Boolean.parseBoolean(parts[3]);
+
+                String dateString = parts[4];
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+                LocalDate intakeDate;
+
+                try {
+                    intakeDate = LocalDate.parse(dateString, formatter);
+                } catch (DateTimeParseException e) {
+                    log.error("Invalid date. {}", e.getMessage());
+                    return;
+                }
+
+                allAnimals.add(new Animal(name, animal, age, adopted, intakeDate));
+            });
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
         // 3) Skip malformed rows and log warnings.
         // 4) Allow blank age as unknown (null), but reject non-numeric age values.
         // 5) Parse intakeDate using DateTimeFormatter.
