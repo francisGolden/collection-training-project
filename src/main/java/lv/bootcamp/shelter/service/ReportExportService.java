c5a27528919d01ce5042e458d68d3b4187a40f28
Francesco Doria <doria5@live.it>
2026-07-02 07:45:29 +0300

    start with step 4

--- src/main/java/lv/bootcamp/shelter/service/ReportExportService.java
+++ src/main/java/lv/bootcamp/shelter/service/ReportExportService.java
@@ -2,7 +2,11 @@ package lv.bootcamp.shelter.service;
 
 import lv.bootcamp.shelter.service.data.ShelterReportData;
 
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 
 public class ReportExportService {
 
@@ -14,6 +18,20 @@ public class ReportExportService {
         // 4) Include oldest animal per species.
         // 5) Include animalsNeedingVetInput as name(species), name2(species2).
         // 6) Use UTF-8 and try-with-resources.
+        Path path = Path.of("upload-report.txt");
+        try {
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+                System.out.println("Empty file created successfully!");
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (FileAlreadyExistsException e) {
+            System.err.println("File already exists: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("IO Exception " + e.getMessage());
+        }
+
 
         throw new UnsupportedOperationException("TODO: implement report export");
     }
